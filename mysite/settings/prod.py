6a6735a2aca1dd2d857e0cbf8ebe23062e0e7982
yoongyo <jyg0172@naver.com>
2019-03-20 22:25:25 +0900

    Adf

--- mysite/settings/prod.py
+++ mysite/settings/prod.py
@@ -4,5 +4,5 @@ import dj_database_url
 db_from_env = dj_database_url.config(env='DATABASE_URL', conn_max_age=5000)
 
 DATABASES['default'].update(db_from_env)
-ALLOWED_HOSTS = ['https://roomescape1.herokuapp.com', 'roomescape1.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 DEBUG = False
