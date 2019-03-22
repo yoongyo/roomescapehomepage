a3e7cf9b3923079901940aba5c6fcd21f5aabf9a
yoongyo <jyg0172@naver.com>
2019-03-22 11:41:18 +0900

    ad

--- mysite/settings/prod.py
+++ mysite/settings/prod.py
@@ -1,8 +1,8 @@
 from .common import *
-import dj_database_url
-
-db_from_env = dj_database_url.config(env='DATABASE_URL', conn_max_age=5000)
-
-DATABASES['default'].update(db_from_env)
+# import dj_database_url
+#
+# db_from_env = dj_database_url.config(env='DATABASE_URL', conn_max_age=5000)
+#
+# DATABASES['default'].update(db_from_env)
 ALLOWED_HOSTS = ['*']
 DEBUG = False
