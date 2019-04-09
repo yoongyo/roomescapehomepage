e4ad9b6e6ecc087b647cca9ae04d212de380c41d
yoongyo <jyg0172@naver.com>
2019-04-09 20:23:10 +0900

    ad

--- mysite/settings/common.py
+++ mysite/settings/common.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'froala_editor',
     'FAQ',
     'booking',
+    'storages'
 ]
 
 MIDDLEWARE = [
--- mysite/settings/prod.py
+++ mysite/settings/prod.py
@@ -1,8 +1,20 @@
 from .common import *
-# import dj_database_url
-#
-# db_from_env = dj_database_url.config(env='DATABASE_URL', conn_max_age=5000)
-#
-# DATABASES['default'].update(db_from_env)
+
 ALLOWED_HOSTS = ['*']
 DEBUG = False
+
+
+AWS_ACCESS_KEY_ID = 'AKIAVNC3EVGMGNL5BLSR'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'bucheonroute'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'static'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'mysite/static'),
+]
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
